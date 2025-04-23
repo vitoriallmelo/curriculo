0bd6c6de66979c2f6c77fd5aaae6dd473a591d8f
Vitória Melo <vluizalmelo@gmail.com>
2025-04-23 02:38:05 -0300

    Adicionar certificados e modificar README

--- src/Certificacoes.jsx
+++ src/Certificacoes.jsx
@@ -6,12 +6,55 @@ function Certificacoes (){
     return(
         <div className={styles.certificacoes}>
             <h2 className={styles.titulo}>Certificações</h2>
+
             <h3 className={styles.tituloTopico}>Front-end</h3>
+                <a href='https://app.rocketseat.com.br/certificates/8fd2ad66-7074-4c3c-b60d-4635eae886f9'>
+                    <img className={styles.horizontais} src='public/react.png' title='Clique para verificar a autenticidade'/>
+                </a>
+                <a href='https://cursos.alura.com.br/certificate/24514763-db8d-4430-bb3a-d059c0b46123'>
+                    <img className={styles.horizontais} src='public/JS2alura.png' title='Clique para verificar a autenticidade'/>
+                </a>
+                <a href='https://cursos.alura.com.br/certificate/382f723c-0d60-49a1-b1d8-9a43a53ae3f4'>
+                    <img className={styles.horizontais} src='public/JS1alura.png' title='Clique para verificar a autenticidade'/>
+                </a>
+                <a href='https://cursos.alura.com.br/certificate/7914b9fd-9207-47b7-b45d-472b2ea74afc'>
+                    <img className={styles.horizontais} src='public/html.png' title='Clique para verificar a autenticidade'/>
+                </a>
             <h3 className={styles.tituloTopico}>Back-end</h3>    
+                <a href='https://cursos.alura.com.br/certificate/b5f139e0-fe25-4b6d-b907-b45aa351cf4f?lang'>
+                    <img className={styles.horizontais}  src='public/JS6alura.png' title='Clique para verificar a autenticidade'/>
+                </a>
+                <a href='https://cursos.alura.com.br/certificate/598ccde5-0c70-4b1d-9e9f-ad2a4b74314c?lang'>
+                    <img className={styles.horizontais} src='public/JS5alura.png' title='Clique para verificar a autenticidade'/>
+                </a>
+                <a href='https://cursos.alura.com.br/certificate/a32f657d-2a83-49eb-a28f-57020a308267?lang'>
+                    <img className={styles.horizontais} src='public/JS4alura.png' title='Clique para verificar a autenticidade'/>
+                </a>
+                <a href='https://cursos.alura.com.br/certificate/c401241c-9c22-4425-9e00-6fb35aca4131'>
+                    <img className={styles.horizontais} src='public/JS3alura.png' title='Clique para verificar a autenticidade'/>
+                </a>
             <h3 className={styles.tituloTopico}>Banco de dados</h3>
+
+                <img className={styles.horizontais} src='public/modelagemDados.png' title='Credencial: 81667030-5F33-4DB5-BA4D-C8C71D2CFC76'/>
+                
             <h3 className={styles.tituloTopico}>Inglês intermediário</h3>
+                
+                <img className={styles.verticais} src='public/ingles.png' />
+
             <h3 className={styles.tituloTopico}>Versionamento de código</h3>
+                <a href='https://cursos.alura.com.br/certificate/de94fbc9-73ec-4b33-ad64-976c2d23fe45?lang'>
+                    <img className={styles.horizontais} src='public/git.png' title='Clique para verificar a autenticidade'/>
+                </a>
             <h3 className={styles.tituloTopico}>Outras conquistas</h3>
+                <a href='https://enem.inep.gov.br/participante/#!/autenticidade'>
+                    <img className={styles.quadrados} src='public/enem2023.png' title='Chave de validação: j5+KbKrfR4+Mln+z0oBy3w=='/>
+                </a>
+                <a href='https://enem.inep.gov.br/participante/#!/autenticidade'>
+                    <img className={styles.quadrados} src='public/enem2022.png' title='Chave de validação: VFoRgTnk/tQFbwsofeqQZA=='/>
+                </a>
+                <a href='https://enem.inep.gov.br/participante/#!/autenticidade'>
+                    <img className={styles.quadrados} src='public/enem2021.png' title='Chave de validação: n3En5eJjSJIuHesBE+IW+g=='/>
+                </a>
         </div>
     )
 }
--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -7,14 +7,17 @@ function Sobre (){
             <p className={styles.textosCentralizados}>Estágio para desenvolvedor/ Desenvolvedor Junior</p>
             <h2 className={styles.titulo}>Principais áreas de interesse</h2>
             <p className={styles.textosCentralizados}>Desenvolvimento Back-end: Java, JavaScript/Node.js</p>
-            <p className={styles.textosCentralizados}>Desenvolvimento fullstack</p>
+            <p className={styles.textosCentralizados}>Desenvolvimento FullStack</p>
             
             <h2 className={styles.titulo}>Sobre</h2>
             <p className={styles.textosJustificados}>Meu nome é Vitória, tenho 21 anos e estou no 2º período de Análise e Desenvolvimento de Sistemas pelo Embarque Digital - programa de bolsas de estudo e formação em tecnologia pelo Porto Digital (Recife - PE). </p>
             <p className={styles.textosJustificados}>Sou técnica em química, pelo IFPE, em transição de carreira para me tornar desenvolvedora. Portanto, utilizo hoje as habilidades analíticas e capacidade de aprofundamento teórico exercitados na minha formação anterior. </p>
-            <p className={styles.textosJustificados}>Ultimamente, foco no estudo de JavaScript/Node.js e, por meio deles, lógica de programação; já tive contato com linguagem C, React, metodologias ágeis e banco de dados academicamente, assim como HTML e CSS (com os quais publiquei o projeto de um site no GitHub - nele, explorarei diversos recursos da documentação do HTML no site W3C). </p>
+            <p className={styles.textosJustificados}>Ultimamente, foco no estudo de Java, JavaScript/Node.js e, por meio deles, lógica de programação; já tive contato com linguagem C, React, metodologias ágeis e banco de dados academicamente, assim como HTML e CSS (com os quais publiquei o projeto de um site no GitHub - nele, explorarei diversos recursos da documentação do HTML no site W3C). </p>
             <p className={styles.textosJustificados}>Ademais, tenho aptidão quanto a comunicação oral e escrita com clareza e qualidade, pois meu esforço nessa área me proporcionou as notas 900, 920 e 940 na redação do ENEM (2021 a 2023). Também estudo inglês há alguns anos e meu nível é classificado como intermediário.</p>
             <p className={styles.textosJustificados}>Em suma, sou uma pessoa focada nos estudos e resultados, de fácil convivência, gosto de aprender coisas novas e busco me aprimorar enquanto profissional. Viso o estágio para evoluir na área e proporcionar bons frutos à minha equipe. </p>
+        
+            <h2 className={styles.titulo}>Habilidades</h2>
+            <p className={styles.textosJustificados}>HTML | CSS | JavaScript | Node.js | Java | JSON | Git | GitHub | React | C | React | Lógica de programação | Banco de Dados | Inglês intermediário | Comunicação | Habilidades analíticas | Trabalho em equipe | Metodologias ágeis | Kanban | Programação Orientada a Objetos</p>
         </div>
     )
 }
